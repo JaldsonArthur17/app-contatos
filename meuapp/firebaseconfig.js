0579af4deaa6eb0c2c49dd6a1ca54830471ec7c8
jaldsonarthur17 <jaldsonarthur1322@gmail.com>
2026-09-14 20:12:24 -0300

    criação de novas pastas e arquivos

---
+++ meuapp/firebaseconfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA_zGhxt9TDlJPe1G6_NBmxLB5xwIVn_lg",
+  authDomain: "app-contatos-b19f5.firebaseapp.com",
+  projectId: "app-contatos-b19f5",
+  storageBucket: "app-contatos-b19f5.firebasestorage.app",
+  messagingSenderId: "233715278980",
+  appId: "1:233715278980:web:3d61e056152b3236554ed1",
+  measurementId: "G-K0EMBQSVXY"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export default app;
